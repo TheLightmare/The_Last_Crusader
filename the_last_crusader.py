9d9d5542eb2a0bbe95ef49569b71e0e37d3feb8b
TheLightmare <47152899+TheLightmare@users.noreply.github.com>
2019-12-10 21:13:46 +0100

    code minimal
    est censé afficher le sprite du croisé dans une fenêtre

--- the_last_crusader.py
+++ the_last_crusader.py
@@ -1,2 +1,34 @@
 import pygame
 
+def main():
+     
+    
+    pygame.init()
+    
+    pygame.display.set_caption("The Last Crusader")
+     
+
+    
+    screen = pygame.display.set_mode((1000,800))
+     
+    running = True
+     
+    
+    while running:
+
+        player = pygame.image.load("./sprites/crusader.png") # charge l'image "crusader.png" depuis le dossier "sprites" dans la variable "player"
+        screen.blit(player, (10,10))
+
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                
+                running = False
+     
+     
+
+if __name__=="__main__":
+    
+    main()
