ab69d50641d50c5c37a8e18ce1f7552415fda98a
TheLightmare <47152899+TheLightmare@users.noreply.github.com>
2019-12-11 09:40:13 +0100

    transparence gérée !

--- the_last_crusader.py
+++ the_last_crusader.py
@@ -9,11 +9,11 @@ def main():
      
 
     
-    screen = pygame.display.set_mode((1000,800), pygame.RESIZABLE, pygame.SCALED) # une fenêtre
+    screen = pygame.display.set_mode((1000,800), pygame.RESIZABLE) # une fenêtre
 
 
     player = pygame.image.load("./sprites/crusader.png").convert() # charge l'image "crusader.png" depuis le dossier "sprites" dans la variable "player"
-    player.set_colorkey((0,255,0)) # TODO : rendre la couleur verte (0,255,0) transparente
+    player.set_colorkey((0,255,0)) #rend la couleur verte (0,255,0) transparente
         
     screen.blit(player, (50,50)) # place le sprite à l'écran aux coordonnées 10,10 
 
